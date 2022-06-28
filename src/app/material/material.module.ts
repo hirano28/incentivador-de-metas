ac7974ee52e8616f52abef59f04f789363341ca0
Edson Hirano <edson.hirano28@gmail.com>
2022-06-27 21:00:10 -0300

    adding material modules

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ],
   exports: [
-    CommonModule
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
